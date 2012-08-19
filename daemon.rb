3d6881a9047337db25b30f9bb7eb267c7d7c21b0
Mike Bannister <notimpossiblemike@gmail.com>
2012-08-19 16:10:19 -0400

    fix path

--- daemon.rb
+++ daemon.rb
@@ -1,8 +1,9 @@
+require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
 
 Daemons.run_proc('remix_api.rb', {:dir_mode => :normal, :dir => "/home/mike/deploy/remix" }) do
   Dir.chdir(pwd)
-  exec "ruby remix_api.rb"
+  exec "ruby /home/mike/deploy/remix/remix_api.rb"
 end

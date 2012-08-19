46239f5e93a69e33d1b0a9a78d653f010b1456d2
Mike Bannister <notimpossiblemike@gmail.com>
2012-08-19 16:05:07 -0400

    fix daemon script

--- daemon.rb
+++ daemon.rb
@@ -2,7 +2,7 @@ require 'daemons'
 
 pwd = Dir.pwd
 
-Daemons.run_proc('phone_sinatra.rb', {:dir_mode => :normal, :dir => "/home/mike/deploy/remix" }) do
+Daemons.run_proc('remix_api.rb', {:dir_mode => :normal, :dir => "/home/mike/deploy/remix" }) do
   Dir.chdir(pwd)
   exec "ruby remix_api.rb"
 end

f446fd146b29e865210d8bd19460ad3d2b9da675
Mike Bannister <notimpossiblemike@gmail.com>
2012-08-19 16:16:03 -0400

    debugging

--- daemon.rb
+++ daemon.rb
@@ -3,7 +3,8 @@ require 'daemons'
 
 pwd = Dir.pwd
 
-Daemons.run_proc('remix_api.rb', {:dir_mode => :normal, :dir => "/home/mike/deploy/remix" }) do
+Daemons.run_proc('remix_api.rb', {:dir_mode => :normal, :dir => "/home/mike/deploy/remix", :log_output => true }) do
+  p pwd
   Dir.chdir(pwd)
   exec "ruby /home/mike/deploy/remix/remix_api.rb"
 end
